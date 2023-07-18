2dc8d6b0497e63b553b8ecd2f1dc400f53e242a2
Matthew Harley English <matthewenglish16@gmail.com>
2023-07-18 15:18:05 +0900

    added some pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// create getComputerChoice function
+// randomly return either rock, paper or scissors
 
+// create playerSelection function
+// take user input (caseinsensitive) for Rock/Paper/Scissors
+
+// write function to play a round of game. two parameters: playerSelection, computerSelection
+// return string with win/lose/draw
+
+// game funtion holding previous function
+// play a 5 round game- loop; console.log to print results and winner each round
