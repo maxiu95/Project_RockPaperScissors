7cca71bedf41ce024616c075d95e66c28ed895d4
Matthew Harley English <matthewenglish16@gmail.com>
2023-07-19 13:17:10 +0900

    finished coding the game. It works as should. Denies invalid inputs, plays 5 rounds and keeps score

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-// create getComputerChoice function
-// randomly return either rock, paper or scissors
+const getComputerChoice = () => {
+    const ranNum = Math.floor(Math.random() * 3);
 
-// create playerSelection function
-// take user input (caseinsensitive) for Rock/Paper/Scissors
+    switch (ranNum) {
+        case 0:
+            return "ROCK";
+            break;
+        case 1:
+            return "SCISSORS";
+            break;
+        case 2:
+            return "PAPER";
+            break;
+    }
+}
 
-// write function to play a round of game. two parameters: playerSelection, computerSelection
-// return string with win/lose/draw
+const getPlayerChoice = () => {
+    while (true) {
+        const playerChoice = prompt("Rock/Paper/Scissors?: ").toUpperCase();
+        if ((playerChoice != "ROCK") && (playerChoice != "SCISSORS") && (playerChoice != "PAPER")) {
+            console.log("Invalid input. Please choose: rock, paper or scissors.");
+        } else {
+            return playerChoice;
+        }
+    }
+}
 
-// game funtion holding previous function
-// play a 5 round game- loop; console.log to print results and winner each round
+const roundOfGame = (computer, player) => {
+    if (player === computer) {
+        return "Draw!";
+    } else if ((computer === "ROCK" && player === "SCISSORS") || 
+        (computer === "SCISSORS" && player === "PAPER") || 
+        (computer === "PAPER" && player === "ROCK")) {
+        return "Lose!";
+    } else {
+        return "Win!";
+    }
+}
+
+const game = () => {
+    let ROUNDS = 0;
+    let SCORE = 0;
+
+    while (ROUNDS < 5) {
+        const computer = getComputerChoice();
+        const player = getPlayerChoice();
+        round = roundOfGame(computer, player);
+        console.log(round);
+        
+        if (round === "Win!") {
+            SCORE++
+        }
+
+        ROUNDS++;
+    }
+    console.log(`Your score is... ${SCORE}!!`);
+}
+
+game();
